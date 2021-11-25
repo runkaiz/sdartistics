385f36f976ae023980b6ebeabca76c56c7015f16
Yaoyu Chen <yaoyuchen@eml.cc>
2021-11-24 21:14:01 -0800

    Add descriptive stat functions

--- lib/src/Statistics/Calculator.dart
+++ lib/src/Statistics/Calculator.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
 /// A class encapsulating read-only statistical calculations.
-class Calculator {
+class SampleDescriptives {
   static double mean(List<double> input) {
     int count = input.length;
     double result = sum(input);
@@ -39,5 +41,168 @@ class Calculator {
     }
 
     return largestValue;
+  }
+
+  static double median(List<double> input) {
+    var sortedList = input.toList();
+    sortedList.sort();
+
+    int count = sortedList.length;
+
+    if (count % 2 == 0) {
+      // Even number of elements
+      double result =
+          (sortedList[count ~/ 2] + sortedList[(count ~/ 2) - 1]) / 2;
+
+      return result;
+    } else {
+      // Odd number of elements
+      double result = sortedList[(count - 1) ~/ 2];
+
+      return result;
+    }
+  }
+
+  /// Find the mode of a list of numbers.
+  /// Return a list of modes. If there are no modes, return an empty list.
+  static List<double> mode(List<double> input) {
+    var counts = <double, int>{};
+    var modes = <double>[];
+
+    for (var i = 0; i < input.length; i++) {
+      var value = input[i];
+      if (counts.containsKey(value)) {
+        counts[value] = counts[value]! + 1;
+      } else {
+        counts[value] = 1;
+      }
+    }
+
+    var maxCount = 0;
+    for (var value in counts.keys) {
+      if (counts[value]! > maxCount) {
+        maxCount = counts[value]!;
+      }
+    }
+
+    for (var value in counts.keys) {
+      if (counts[value] == maxCount) {
+        modes.add(value);
+      }
+    }
+
+    return modes;
+  }
+
+  /// Calculate the frequency of values in a list, return a map of values and their frequencies
+  /// The list accepts a list of numbers or strings, but the map will be keyed by the string representation of the number
+  static Map<String, int> frequency(List<dynamic> input) {
+    Map<String, int> result = {};
+
+    for (var i = 0; i < input.length; i++) {
+      var current = input[i];
+      var currentCount = 1;
+
+      for (var j = i + 1; j < input.length; j++) {
+        if (current == input[j]) {
+          currentCount++;
+        }
+      }
+
+      result[current.toString()] = currentCount;
+    }
+
+    return result;
+  }
+
+  static double variance(List<double> input) {
+    double meanValue = mean(input);
+    double result = 0;
+
+    for (var i = 0; i < input.length; i++) {
+      result += pow((input[i] - meanValue), 2);
+    }
+
+    result = result / (input.length - 1);
+
+    return result;
+  }
+
+  static double standardDeviation(List<double> input) {
+    double meanValue = mean(input);
+    double sumOfSquaredDifferences = 0.0;
+
+    for (var i = 0; i < input.length; i++) {
+      sumOfSquaredDifferences += pow((input[i] - meanValue), 2);
+    }
+
+    double result = sqrt(sumOfSquaredDifferences / (input.length - 1));
+
+    return result;
+  }
+
+  static double coefficientOfVariation(List<double> input) {
+    double standardDeviationValue = standardDeviation(input);
+    double meanValue = mean(input);
+    double result = standardDeviationValue / meanValue;
+
+    return result;
+  }
+
+  static double skewness(List<double> input) {
+    double meanValue = mean(input);
+    double sd = standardDeviation(input);
+    double result = 0;
+
+    for (var i = 0; i < input.length; i++) {
+      result += pow((input[i] - meanValue) / sd, 3);
+    }
+
+    double step_1 = input.length / (input.length - 1) * 1 / (input.length - 2);
+    result = step_1 * result;
+
+    return result;
+  }
+
+  static double kurtosis(List<double> input) {
+    double meanValue = mean(input);
+    double sd = standardDeviation(input);
+    double result = 0;
+
+    for (var i = 0; i < input.length; i++) {
+      result += pow((input[i] - meanValue) / sd, 4);
+    }
+
+    double step_1 = input.length /
+        (input.length - 1) *
+        (input.length + 1) /
+        (input.length - 2) *
+        1 /
+        (input.length - 3);
+    result = step_1 * result;
+
+    return result;
+  }
+
+  /// SPSS reports kurtosis excess as kurtosis.
+  static double kurtosisExcess(List<double> input) {
+    double kurtosisValue = kurtosis(input);
+    double result = 0;
+
+    double step_1 = 3 *
+        pow((input.length - 1), 2) /
+        (input.length - 2) /
+        (input.length - 3);
+
+    result = kurtosisValue - step_1;
+
+    return result;
+  }
+
+  static double standardError(List<double> input) {
+    double standardDeviationValue = standardDeviation(input);
+    double result = standardDeviationValue / sqrt(input.length);
+
+    return result;
   }
 }
--- test/sdartistics_test.dart
+++ test/sdartistics_test.dart
@@ -85,20 +85,106 @@ void main() {
     });
   });
 
-  group('Calculator tests', () {
-    List<double> testList = [1.2, 1.3, 1.4];
+  group('Descriptive statistics tests', () {
+    double _round(double value) {
+      return (value * 100).round() / 100;
+    }
+
+    List<double> testList = [
+      1.00,
+      1.03,
+      1.42,
+      1.39,
+      2.10,
+      0.86,
+      0.98,
+      1.36,
+      0.86,
+      1.38,
+      -0.65,
+      -0.20,
+      0.75,
+      -1.53,
+      1.62,
+      0.49,
+      0.75,
+      0.51,
+      0.60,
+      3.25
+    ];
+
+    Map<String, dynamic> testDescriptives = {
+      'count': 20,
+      'sum': 17.97,
+      'max': 3.25,
+      'min': -1.53,
+      'mean': 0.8985,
+      'median': 0.92,
+      'mode': [0.86, 0.75],
+      'variance': 0.97,
+      'standardDeviation': 0.99,
+      'skewness': -0.24,
+      'kurtosis': 5.77,
+      'kurtosisExcess': 2.23,
+      'coefficientOfVariation': 1.10,
+      'standardError': 0.22,
+    };
 
     test('Test sum', () {
-      expect(Calculator.sum(testList), 3.9);
+      expect(SampleDescriptives.sum(testList), testDescriptives['sum']);
     });
     test('Test mean', () {
-      expect(Calculator.mean(testList), 1.3);
+      expect(SampleDescriptives.mean(testList), testDescriptives['mean']);
     });
     test('Test min', () {
-      expect(Calculator.min(testList), 1.2);
+      expect(SampleDescriptives.min(testList), testDescriptives['min']);
     });
     test('Test max', () {
-      expect(Calculator.max(testList), 1.4);
+      expect(SampleDescriptives.max(testList), testDescriptives['max']);
+    });
+
+    test('Test median', () {
+      expect(_round(SampleDescriptives.median(testList)),
+          testDescriptives['median']);
+    });
+
+    test('Test mode', () {
+      expect(SampleDescriptives.mode(testList), testDescriptives['mode']);
+    });
+
+    test('Test variance', () {
+      expect(_round(SampleDescriptives.variance(testList)),
+          testDescriptives['variance']);
+    });
+
+    test('Test standard deviation', () {
+      expect(_round(SampleDescriptives.standardDeviation(testList)),
+          testDescriptives['standardDeviation']);
+    });
+
+    test('Test skewness', () {
+      expect(_round(SampleDescriptives.skewness(testList)),
+          testDescriptives['skewness']);
+    });
+
+    test('Test kurtosis', () {
+      expect(_round(SampleDescriptives.kurtosis(testList)),
+          testDescriptives['kurtosis']);
+    });
+
+    test('Test kurtosis excess', () {
+      expect(_round(SampleDescriptives.kurtosisExcess(testList)),
+          testDescriptives['kurtosisExcess']);
+    });
+
+    test('Test coefficient of variation', () {
+      expect(_round(SampleDescriptives.coefficientOfVariation(testList)),
+          testDescriptives['coefficientOfVariation']);
+    });
+
+    test('Test standard error', () {
+      expect(_round(SampleDescriptives.standardError(testList)),
+          testDescriptives['standardError']);
     });
   });
 }
